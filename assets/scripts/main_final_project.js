0541bedc974cace0b50f387e0756b6b43c409314
Christine Cunniff <christinecunniff@gmail.com>
2015-07-09 00:08:11 -0400

    style changes, added js

--- assets/scripts/main_final_project.js
+++ assets/scripts/main_final_project.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 
 //what do you want to do? USE PSUEDOCODE YA FREAK
 
+//MAKE A DONATE BUTTON ON about.html
+$( "#donate" ).click(function() {
+  alert( "Thank you for donating to Skeletix <3" );
+  console.log("cha-CHING $$$");
+});
 
-
-
-
-
-
+$(".main-nav").hide();
+	$(".main-nav").fadeIn(1000);
 
 
 
